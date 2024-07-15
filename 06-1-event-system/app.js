f72a211e848ce791ff0d83958fa69ee9bb939089
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-16 00:38:59 +0700

    answer event system

--- 06-1-event-system/app.js
+++ 06-1-event-system/app.js
@@ -1,8 +1,21 @@
 const EventEmitter = require('events');
 
+const eventEmitter = new EventEmitter();
+
 const BasicMath = {
   add: (a, b) => console.log(a + b),
   subtract: (a, b) => console.log(a - b),
 };
 
 // Tulis kodemu di bawah ini ...
+// fungsi add dari basicmath sebagai listener dari event calculate
+eventEmitter.on('calculate', add);
+
+// fungsi subtract dari basicmath sebagai listener dari event calculate
+eventEmitter.on('calculate', subtract);
+
+// bangkitkan event calculate dengan parameter angka 2 dan 3
+eventEmitter.emit('calculate', 2, 3);
+
+// bangkitkan event calculate dengan parameter angka 5 dan 6
+eventEmitter.emit('calculate', 5, 6);
