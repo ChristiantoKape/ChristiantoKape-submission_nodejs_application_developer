014af9ad7da09098b7c9a80ee8e9f15b54e920ca
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-22 16:05:35 +0700

    answer piping stream

--- 09-2-piping-stream/app.js
+++ 09-2-piping-stream/app.js
@@ -15,3 +15,8 @@ const { createReadable, createWritable } = require('./utils');
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
+const readable = createReadable(alphabet);
+
+const writable = createWritable();
+
+readable.pipe(writable);
