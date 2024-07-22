a013bd0a9e1e1d0eceb8e572ff8a2a5dc67e6e88
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-22 22:13:37 +0700

    answer os uptime and momory

--- 11-2-os-uptime-and-memory/app.js
+++ 11-2-os-uptime-and-memory/app.js
@@ -1,6 +1,12 @@
+const os = require('os');
+
 setTimeout(() => {
-  console.log(null); // @TODO cetak uptime dari process
-  console.log(null); // @TODO cetak uptime dari sistem operasi
-  console.log(null); // @TODO cetak total memori yang tersedia di sistem operasi
-  console.log(null); // @TODO cetak total heap memori
+  // @TODO cetak uptime dari process
+  console.log(process.uptime());
+  // @TODO cetak uptime dari sistem operasi
+  console.log(os.uptime());
+  // @TODO cetak total memori yang tersedia di sistem operasi
+  console.log(os.totalmem()); 
+  // @TODO cetak total heap memori
+  console.log(process.memoryUsage().heapTotal); 
 }, 1000);
